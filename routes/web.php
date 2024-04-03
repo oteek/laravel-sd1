f623a72b9ff19e41a4bcaab86bf25a4dcd78e9d6
Mawskeeto1223 <tomaxx11@gmail.com>
2024-04-03 15:16:44 +0300

    laravel/ui auth

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    //return view('welcome');
-    return 'test';
+    return view('welcome');
+    //return 'test';
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
