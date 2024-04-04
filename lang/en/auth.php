392c734811536400b294ad9925dc9e232ad6ccd8
Mawskeeto1223 <tomaxx11@gmail.com>
2024-04-04 03:48:39 +0300

    localization

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -16,5 +16,28 @@ return [
     'failed' => 'These credentials do not match our records.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'loggedin' => 'You are logged in!',
+    'login_title' => 'Login',
+    'register_title' => "Register",
 
+    'verify_title' => 'Verify Your Email Address',
+    'verify_p1' => 'A fresh verification link has been sent to your email address.',
+    'verify_p2' => 'Before proceeding, please check your email for a verification link.',
+    'verify_p3' => 'If you did not receive the email',
+    'verify_p4' => 'click here to request another',
+
+    'to_register' => "Register",
+    'to_login' => "Login",
+
+    'l_name' => "Name",
+    'l_email' => "Email Address",
+    'l_pwd' => "Password",
+    'l_cpwd' => "Confirm Password",
+    'l_rpwd' => "Reset Password",
+    'r_sendreset' => "Send Password Reset Link",
+
+    'remember' => "Remember Me",
+    'forgotpwd' => 'Forgot Your Password?',
+
+    'confirmpwd' => "Please confirm your password before continuing."
 ];
