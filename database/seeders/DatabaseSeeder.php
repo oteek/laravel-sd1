e60717b7808288615b546e71b421fa44aa7765e0
Mawskeeto1223 <tomaxx11@gmail.com>
2024-04-18 23:37:52 +0300

    db seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,16 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
+use App\Models\Attending;
+use App\Models\City;
+use App\Models\Comment;
+use App\Models\Country;
+use App\Models\Event;
+use App\Models\Gallery;
+use App\Models\Like;
+use App\Models\SavedEvent;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +24,27 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+            'password' => 'password',
+        ]);
+        
+        Country::create(['name' => 'United Kingdom']);
+        Country::create(['name' => 'Greece']);
+        Country::create(['name' => 'Lithuania']);
+        City::create(['country_id' => 1, 'name' => 'London']);
+        City::create(['country_id' => 1, 'name' => 'Liverpool']);
+        City::create(['country_id' => 1, 'name' => 'Leicester']);
+        City::create(['country_id' => 2, 'name' => 'Athens']);
+        City::create(['country_id' => 2, 'name' => 'Patra']);
+        City::create(['country_id' => 2, 'name' => 'Zakynthos']);
+        City::create(['country_id' => 3, 'name' => 'Vilnius']);
+        City::create(['country_id' => 3, 'name' => 'Kaunas']);
+        City::create(['country_id' => 3, 'name' => 'Klaipėda']);
+
+        Tag::create(['name' => 'Laravel', 'slug' => 'laravel']);
+        Tag::create(['name' => 'Vue JS', 'slug' => 'vue-js']);
+        Tag::create(['name' => 'Livewire', 'slug' => 'livewire']);
     }
 }
